259aee12b569b53c6ad6f87fa7f0ca7065661660
Cengizhan Boz <bozcengizhan2@gmail.com>
2026-03-30 19:02:04 +0300

    Zamanlama hatası düzeltildi.

--- app/src/main/java/com/example/todolistforprojects/Repositories/TaskRepository.kt
+++ app/src/main/java/com/example/todolistforprojects/Repositories/TaskRepository.kt
@@ -67,7 +67,7 @@ class TaskRepository {
         val failedRef = db.collection("failedTasks").document(task.id)
         val expireDate = com.google.firebase.Timestamp.now() // şimdi
             .toDate()
-            .apply { time +=  30 * 1000 } // 30sn sonra siliniyor
+            .apply { time += 7L * 24 * 60 * 60 * 1000 } // 30sn sonra siliniyor
 
         val taskMap = hashMapOf(
             "id" to task.id,
